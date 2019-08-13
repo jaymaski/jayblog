32320e68bc0c2753b6b182609d2d90b7d3f82dce
John Joshua Macareñas <jjaymascarenas@gmail.com>
2019-08-13 09:16:06 +0800

    Update - Home Page UI

--- resources/views/viewers/home.blade.php
+++ resources/views/viewers/home.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.layout') 
-@section('content')
-
-
-
+@section('landing-page')
+    <div class="home">
+        <div class="home__title">
+            <p class="popout">
+                <span>H</span>
+                <span>I</span>
+                <span>,</span><br>
+                <span>I</span>
+                <span>'</span>
+                <span>M</span>
+                <span>J</span>
+                <span>A</span>
+                <span>Y</span>
+                <span>!</span>
+            </p>
+        </div>
+        
+        <div class="home__image">
+            <img class="home__image-cover" src="{{URL::asset('/images/parts/jay-bnw.png')}}" alt="background_image" />
+        </div>
+    </div>
 @endsection
 
 @section('sidebar')
